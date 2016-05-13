c4797a27790e40fc984828c9ef2344b0dcb648ea
informatica <gomezcarlose@gmail.com>
2016-05-12 23:27:28 -0400

    First commit

---
+++ gem/src/ve/com/gem/model/entity/Basic.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package ve.com.gem.model.entity;
+
+import java.sql.Timestamp;
+
+/**
+ * @author informatica
+ *
+ */
+//@Embeddable
+public class Basic {
+
+	private String name;
+	private String description;
+	private Timestamp created;
+	private Timestamp updated;
+	private Boolean isActive;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Timestamp getCreated() {
+		return created;
+	}
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
+	public Timestamp getUpdated() {
+		return updated;
+	}
+	public void setUpdated(Timestamp updated) {
+		this.updated = updated;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return this.name;
+	}
+}
