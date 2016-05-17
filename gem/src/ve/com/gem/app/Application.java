7ca25d34d1fffbebbe54013cdc682744a22e7908
informatica <gomezcarlose@gmail.com>
2016-05-17 15:53:34 -0400

    fix project

---
+++ gem/src/ve/com/gem/app/Application.java
@@ -0,0 +1,16 @@
+package ve.com.gem.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+	}
+
+}
