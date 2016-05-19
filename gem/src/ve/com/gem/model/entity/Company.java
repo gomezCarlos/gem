b605838917b6dd565c19fec0b07ff75d1d6e32ee
Ajru10 <anraur_10@msn.com>
2016-05-18 23:38:26 -0430

    Se modifico la clase Company se elimino name y id ya que el id lo hereda de Base.

--- gem/src/ve/com/gem/model/entity/Company.java
+++ gem/src/ve/com/gem/model/entity/Company.java
@@ -5,31 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 /** Created by Andres & Ronaldo Team (4)
 */
 @Entity
 public class Company extends Base {
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	private String name;
 	private String customer;
 	public Company(long longValue, String name2) {
 		// TODO Auto-generated constructor stub
 	
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
+
 	}
 	public String getCustomer() {
 		return customer;
