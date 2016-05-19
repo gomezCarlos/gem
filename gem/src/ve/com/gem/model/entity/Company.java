769410e23f118d3edddc2b9223de79983074f7e9
informatica <gomezcarlose@gmail.com>
2016-05-19 10:55:02 -0400

    Add Company to persistence.xml

--- gem/src/ve/com/gem/model/entity/Company.java
+++ gem/src/ve/com/gem/model/entity/Company.java
@@ -1,12 +1,7 @@
 package ve.com.gem.model.entity;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+
 /** Created by Andres & Ronaldo Team (4)
 */
 @Entity
